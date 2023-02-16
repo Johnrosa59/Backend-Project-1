855c72a2bde789bd0cb489f0b4882979fc51d85c
johnrosa59 <jvrosatecnologia@gmail.com>
2023-02-16 18:30:23 -0300

    Subindo novas atualizações

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const { request, response } = require('express')
-const express = require('express')
-const uuid = require('uuid')
+const { request, response } = require("express");
+const express = require("express");
+const uuid = require("uuid");
+const cors = require("cors");
 
-const app = express()
-const port = 3000
-app.use(express.json())
+const app = express();
+const port = 3001;
+app.use(express.json());
+app.use(cors());
 
 /*
 
@@ -21,74 +23,64 @@ app.use(express.json())
 
     */
 
-const users = []
+const users = [];
 
 const checkUserId = (request, response, next) => {
+  const { id } = request.params;
 
-    const { id } = request.params
+  const index = users.findIndex((user) => user.id === id);
 
-    const index = users.findIndex(user => user.id === id)
+  if (index < 0) {
+    return response.status(404).json({ message: "User not found" });
+  }
 
-    if (index < 0) {
-        return response.status(404).json({ message: "User not found" })
-    }
+  request.userIndex = index;
+  request.userId = id;
 
-    request.userIndex = index
-    request.userId = id
+  next();
+};
 
-    next()
-}
+app.get("/users", (request, response) => {
+  return response.json(users);
+});
 
+app.post("/users", (request, response) => {
+  try {
+    const { name, age } = request.body;
 
+    if (age < 18) throw new Error("Only allowed users over 18 years old!");
 
-app.get('/users', (request, response) => {
+    const user = { id: uuid.v4(), name, age };
 
-    console.log('A rota foi chamada')
+    users.push(user);
+    return response.status(201).json(user);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  } finally {
+    console.log("Finished all");
+  }
+});
 
-    return response.json(users)
+app.put("/users/:id", checkUserId, (request, response) => {
+  const { name, age } = request.body;
+  const index = request.userIndex;
+  const id = request.userId;
 
-})
+  const updatedUser = { id, name, age };
 
-app.post('/users', (request, response) => {
-    const { name, age } = request.body
+  users[index] = updatedUser;
 
-    const user = { id: uuid.v4(), name, age }
+  return response.json(updatedUser);
+});
 
-    users.push(user)
-    return response.status(201).json(user)
-})
+app.delete("/users/:id", checkUserId, (request, response) => {
+  const index = request.userIndex;
 
-app.put('/users/:id', checkUserId, (request, response) => {
-    const { name, age } = request.body
-    const index = request.userIndex
-    const id = request.userId
+  users.splice(index, 1);
 
-    const updatedUser = { id, name, age }
-
-    users[index] = updatedUser
-
-    return response.json(updatedUser)
-})
-
-app.delete('/users/:id', checkUserId, (request, response) => {
-    const index = request.userIndex
-
-    users.splice(index, 1)
-
-    return response.status(204).json()
-
-})
-
-
-
-
-
-
-
-
-
-
+  return response.status(204).json();
+});
 
 app.listen(port, () => {
-    console.log(`🚀Server started on port ${port} 🚀`)
-})
+  console.log(`🚀Server started on port ${port} 🚀`);
+});
